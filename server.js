4d87ff065673fb5d927245b62361445e85612c73
lixiaof8 <xiaof.li@mail.utoronto.ca>
2025-07-26 20:35:10 -0400

    1.3 version

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server running...');
-});
